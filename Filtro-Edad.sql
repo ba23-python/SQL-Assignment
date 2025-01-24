b5ca5699ad9db6a9132ea1f089645c104a5abdb5
Bilyana Ancheva <153090623+ba23-python@users.noreply.github.com>
2025-01-24 19:03:22 +0100

    Update Filtro-Edad.sql

--- Filtro-Edad.sql
+++ Filtro-Edad.sql
@@ -1,23 +1,23 @@
  Clientes con edades entre 25 y 40 años.
  
 SELECT 
-CodCliente, 
-Nombre, 
-Apellido, 
-DNI, 
-Sexo, 
-CorreoElectronico, 
-Móvil, 
-Año_nacimiento, 
-Ciudad, 
-Seguro_Activo, 
-eguro_Anulado, 
+CodCliente, Nombre, Apellido, DNI, 
+Sexo, CorreoElectronico, Móvil, 
+Año_nacimiento, Ciudad, Seguro_Activo, Seguro_Anulado, 
 Si_Publicidad
 FROM DE_Clientes
 WHERE (YEAR(GETDATE()) - YEAR(Año_nacimiento)) BETWEEN 25 AND 40;
 
+------------------------
 
-** Ojo al utlizar GETDATE() se van respetar los ajustes del servidor de Marketing Cloud respecto a la fecha.
+[EN] Note: When using GETDATE(), the date settings of the Marketing Cloud server will be respected. In this exercise, it is assumed that all these settings are correctly configured.
+
+The result of this query would be a new query data extension with the following data:
+
+Customers aged 25 to 40 years, using the current year to calculate the age based on the customer code, which we assume is the SubscriberKey.
+
+
+[es]** Ojo al utlizar GETDATE() se van respetar los ajustes del servidor de Marketing Cloud respecto a la fecha.
 En este ejercicio se supone que todos estos ajustes estan correctamente.
 
 El resultado de este query seria una nueva query data extension con los siguientes datos:
